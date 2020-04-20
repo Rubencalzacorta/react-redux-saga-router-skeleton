ccd4486395f3228f2572c886c0c955e8f19486cb
Ruben Calzacorta <calzacorta.ruben@gmail.com>
2020-04-20 11:27:34 +0200

    backup commit

--- src/redux/selectors/index.js
+++ src/redux/selectors/index.js
@@ -1,7 +1,6 @@
 import { get } from "lodash"
 
 export const isSearchLoading = state => get(state, "search.isLoading")
-
 //dentro del json de la respuesta hay que seleccionar donde esta informacion que buscan(search.movieResults.Search)
 export const movieResults = state => get(state, "search.movieResults.Search")
 
